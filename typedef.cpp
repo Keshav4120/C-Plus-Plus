12d1f8a516107b20137b2c6dc84e02ab63a817a9
Keshav Jindal <115159037+Keshav4120@users.noreply.github.com>
2024-06-12 23:38:26 +0530

    Add files via upload

---
+++ typedef.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+typedef std::string name_t;
+using num = int;
+
+int main(){
+    name_t name = "Hello World";
+    num a = 199;
+    std::cout << name << '\n';
+    std::cout << a << '\n';
+    
+
+
+}
