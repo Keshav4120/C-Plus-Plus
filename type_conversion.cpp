9d025703b51a81e1ada8560648cba7485fda77ef
Keshav Jindal <115159037+Keshav4120@users.noreply.github.com>
2024-06-12 23:54:48 +0530

    Add files via upload

---
+++ type_conversion.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+    // int x =3.14;
+    // double x =3.14; changing by implicit
+    double x=(int)3.13;//changing by explixit 
+    std::cout<< x<<'\n';    
+    std::cout<< (char)100;    
+}
